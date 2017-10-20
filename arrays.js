b671b76129e9dfa68deaa323363966dc4f7920ac
Remmy Clay <remmy.lauren@gmail.com>
2017-10-20 08:04:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length -1);
+  array.slice(0, array.length-1);
 }
