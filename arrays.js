3d97680456cb51f57be4b8f3e81fea840ca01896
Remmy Clay <remmy.lauren@gmail.com>
2017-10-20 09:01:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array;
 }
 function accessElementInArray(array, index){
+  array = [1, 2, 3];
   return array(2);
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
